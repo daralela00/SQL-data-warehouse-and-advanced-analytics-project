58ccd6c434edd5917f1120088a4a16d7f7d5fe43
daralela00 <92344392+daralela00@users.noreply.github.com>
2025-04-11 18:22:22 +0200

    Update crm_sales_detailsTransform.sql

--- scripts/silver/crm_sales_detailsTransform.sql
+++ scripts/silver/crm_sales_detailsTransform.sql
@@ -30,7 +30,7 @@ CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
 END AS sls_due_dt,
 CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
 		THEN sls_quantity * ABS(sls_price)
-	ELSE sls_price
+	ELSE sls_sales
 END AS sls_sales,
 sls_quantity,
 CASE WHEN sls_price is NULL OR sls_price <= 0
