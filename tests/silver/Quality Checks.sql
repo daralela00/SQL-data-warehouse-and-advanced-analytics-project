199df1ce678eff1149b16dcb22817562f8e37446
daralela00 <92344392+daralela00@users.noreply.github.com>
2025-04-11 17:48:42 +0200

    Update Quality Checks.sql

--- tests/silver/Quality Checks.sql
+++ tests/silver/Quality Checks.sql
@@ -36,6 +36,16 @@ FROM silver.crm_cust_info
 SELECT *
 FROM bronze.crm_prd_info
 where prd_end_dt < prd_start_dt
+
+-- Check for invalid dates
+
+SELECT
+NULLIF(sls_order_dt, 0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0
+OR LEN(sls_order_dt) != 8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
   
 -- Checking whole table 
 SELECT * FROM silver.crm_cust_info
