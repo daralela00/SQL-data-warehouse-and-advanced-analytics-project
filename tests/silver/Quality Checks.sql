6439a367d7b98631ed9adc756953f57d5008c8e8
daralela00 <92344392+daralela00@users.noreply.github.com>
2025-04-11 19:42:29 +0200

    Update Quality Checks.sql

--- tests/silver/Quality Checks.sql
+++ tests/silver/Quality Checks.sql
@@ -67,6 +67,13 @@ WHERE sls_sales != sls_quantity * sls_price
 OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
 OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
 ORDER BY sls_sales, sls_quantity, sls_price
+
+-- Check out of range dates
+
+SELECT DISTINCT
+bdate
+FROM bronze.erp_cust_az12
+WHERE bdate < '1925-01-01' OR bdate > GETDATE()
   
 -- Checking whole table 
 SELECT * FROM silver.crm_cust_info
