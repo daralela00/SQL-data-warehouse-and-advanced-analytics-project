fcd2eaf1528597e5e6ab0fbb7055cedd4370e92a
daralela00 <92344392+daralela00@users.noreply.github.com>
2025-04-11 17:40:31 +0200

    Create crm_prd_infoTransofrm.sql

---
+++ scripts/silver/crm_prd_infoTransofrm.sql
@@ -0,0 +1,31 @@
+/*
+After DDL script for silver layer run this script to transform and load silver table named "silver.crm_prd_info".
+Here we are transorming table "bronze.crm_prd_info" from bronze layer into a table "silver.crm_prd_info" that we need for silver layer.
+*/
+
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,	
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT
+prd_id,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+prd_nm,
+ISNULL(prd_cost, 0) as prd_cost,
+CASE UPPER(TRIM(prd_line))
+	 WHEN 'M' THEN 'Mountain'
+	 WHEN 'R' THEN 'Road'
+	 WHEN 'S' THEN 'Other Sales'
+	 WHEN 'T' THEN 'Touring'
+	 ELSE 'n/a'
+END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt,
+CAST(DATEADD(DAY, -1, LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)) AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
