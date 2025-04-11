2a900a3608f26f07a7156d20ee32925ac051dd64
daralela00 <92344392+daralela00@users.noreply.github.com>
2025-04-11 21:27:24 +0200

    Create erp_loc_a101Transform.sql

---
+++ scripts/silver/erp_loc_a101Transform.sql
@@ -0,0 +1,18 @@
+/*
+After DDL script for silver layer run this script to transform and load silver table named "silver.erp_loc_a101".
+Here we are transorming table "bronze.erp_loc_a101" from bronze layer into a table "silver.erp_loc_a101" that we need for silver layer.
+*/
+
+INSERT INTO silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+
+SELECT
+REPLACE(cid, '-', '') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) in ('US', 'USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	 ELSE cntry
+END AS cntry
+FROM bronze.erp_loc_a101
